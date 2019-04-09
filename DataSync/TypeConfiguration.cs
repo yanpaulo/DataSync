e8c95ce1a4a5e93d4cf9799f87f2f63b5e45ff2a
Yan Paulo <yanpaulo@hotmail.com>
2019-04-09 06:32:33 -0300

    TypeProvider file

--- DataSync/TypeConfiguration.cs
+++ DataSync/TypeConfiguration.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Threading.Tasks;
 
 namespace DataSync
 {
@@ -16,36 +15,5 @@ namespace DataSync
     public class TypeConfiguration
     {
         internal TypeProvider TypeProvider { get; set; } = new TypeProvider();
-    }
-
-    internal class TypeProvider
-    {
-        internal object SyncProvider { get; set; }
-
-        internal async Task Pull<T>()
-        {
-            await Invoke<T>(nameof(ISyncProvider.PullAsync));
-        }
-
-        internal async Task Push<T>()
-        {
-            await Invoke<T>(nameof(ISyncProvider.PushAsync));
-        }
-
-        internal async Task Put<T>()
-        {
-            await Invoke<T>(nameof(ISyncProvider.PutAsync));
-        }
-
-        private Task Invoke<T>(string methodName)
-        {
-            var p = SyncProvider.GetType();
-            var task = p
-                .GetMethod(methodName)
-                .MakeGenericMethod(typeof(T))
-                .Invoke(SyncProvider, null) as Task;
-
-            return task;
-        }
     }
 }
---
+++ DataSync/TypeProvider.cs
@@ -0,0 +1,35 @@
+﻿using System.Threading.Tasks;
+
+namespace DataSync
+{
+    internal class TypeProvider
+    {
+        internal object SyncProvider { get; set; }
+
+        internal async Task Pull<T>()
+        {
+            await Invoke<T>(nameof(ISyncProvider.PullAsync));
+        }
+
+        internal async Task Push<T>()
+        {
+            await Invoke<T>(nameof(ISyncProvider.PushAsync));
+        }
+
+        internal async Task Put<T>()
+        {
+            await Invoke<T>(nameof(ISyncProvider.PutAsync));
+        }
+
+        private Task Invoke<T>(string methodName)
+        {
+            var p = SyncProvider.GetType();
+            var task = p
+                .GetMethod(methodName)
+                .MakeGenericMethod(typeof(T))
+                .Invoke(SyncProvider, null) as Task;
+
+            return task;
+        }
+    }
+}
